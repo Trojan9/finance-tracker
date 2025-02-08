78ee4c4287c9a83fd798cfb2eb855bffdb67b0d5
trojan9 <timmybanjo@gmail.com>
2025-02-08 19:52:27 +0000

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/postcss'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [require('@tailwindcss/postcss')],
+  root: "public",
   css: {
     postcss: './postcss.config.js',
   },
 });
+
